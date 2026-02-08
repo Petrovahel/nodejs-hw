746dd5b7013e672b2dbc5be944babe05de0a8ec4
Olena Petrova <petrovahel@icloud.com>
2026-02-08 17:32:56 +0200

    fix PORT variable in server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
