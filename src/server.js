49281ddadf45990e3ec5e609a5f79b3010c74e09
Olena Petrova <petrovahel@icloud.com>
2026-02-18 18:23:31 +0200

    complete task

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 
@@ -19,14 +20,13 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
+app.use(authRoutes);
 app.use(notesRoutes);
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello world!' });
-});
+app.use(errors());
 
 app.use(notFoundHandler);
-app.use(errors());
+
 app.use(errorHandler);
 
 await connectMongoDB();
@@ -34,8 +34,3 @@ await connectMongoDB();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-app.use(authRoutes);
-app.use(notesRoutes);
-
-
